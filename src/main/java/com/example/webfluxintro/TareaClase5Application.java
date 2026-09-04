3dcdbaccaf109b26d70cb61d7bb68a69d878f1b0
jomcabre <jomcabre@bancolombia.com.co>
2026-09-04 09:08:17 -0500

    Tarea 5

---
+++ src/main/java/com/example/webfluxintro/TareaClase5Application.java
@@ -0,0 +1,13 @@
+package com.example.webfluxintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaClase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaClase5Application.class, args);
+    }
+
+}
